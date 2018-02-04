e59ae57133e5364032c6c64c1161341df6744ce1
liufangzheng <liufangzheng@jd.com>
2018-02-04 20:55:32 +0800

    添加了拦截器

--- src/controller/ManageController.java
+++ src/controller/ManageController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import com.jfinal.aop.Before;
 import com.jfinal.core.Controller;
+import interceptor.UserInterceptor;
 
 public class ManageController extends Controller{
 
+    @Before(UserInterceptor.class)
     public void bussiness(){
         renderJsp("bussiness.jsp");
     }
--- src/interceptor/UserInterceptor.java
+++ src/interceptor/UserInterceptor.java
@@ -9,5 +9,16 @@ public class UserInterceptor implements Interceptor {
     @Override
     public void intercept(Invocation inv) {
         HttpSession session = inv.getController().getSession();
+        if(session == null){
+            inv.getController().redirect("/");
+        }else{
+            String username = (String) session.getAttribute("username");
+            if(username != null){
+                System.out.println(username+"已登录！");
+                inv.invoke();
+            }else{
+                inv.getController().redirect("/");
+            }
+        }
     }
 }
